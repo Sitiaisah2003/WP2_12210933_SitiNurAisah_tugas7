e8b6b0526d89b16300740fca786ecf0d1b519c65
Sitiaisah2003 <113379837+Sitiaisah2003@users.noreply.github.com>
2022-10-30 23:51:10 +0700

    tugas 7

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Pamungkas
+ Saya tingga di Desa Kapur kav 1
+ olah raga yang saya sukai adalah
+ sepakbola";
+ }
+}
